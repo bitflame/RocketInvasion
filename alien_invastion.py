b9189146125cbc69a6d2f6355392c2e2a89979b8
bitflame <monazzam@protonmail.com>
2024-05-18 17:05:36 -0400

    changed the image to a rocket and modified the code so it move up, down, left, and right as stated in exercise 12-4

--- alien_invastion.py
+++ alien_invastion.py
@@ -54,6 +54,10 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
         
     def _check_keyup_events(self, event):
         """Respond to key releases."""
@@ -61,6 +65,10 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
             
 if __name__ == '__main__': #<-Only run if the file is called directly 
     # Make a game instance, and run the game.
--- ship.py
+++ ship.py
@@ -7,14 +7,17 @@ class Ship:
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         # Load the ship images and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/rocket.png')
         self.rect = self.image.get_rect()
         # start each new ship at the bottom center of teh screen.
         self.rect.midbottom = self.screen_rect.midbottom
         # store a float for the ship's exact horizontal position
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
         
     def update(self):
         """Update ship's position based on movement flags"""
@@ -23,9 +26,13 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0: 
             self.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
         #Update rectangle objects
         self.rect.x = self.x
-        
+        self.rect.y = self.y
     def blitme(self):
         """Draw the ship at its current location"""
         self.screen.blit(self.image, self.rect)
